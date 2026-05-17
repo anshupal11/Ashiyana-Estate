6f9b4e9f4ff45be5c4c98b6cd08eeff6c7ad97c3
anshu <anshu.pal108@gmail.com>
2026-05-17 17:09:33 +0530

    setting and req

--- Estate/settings.py
+++ Estate/settings.py
@@ -137,9 +137,10 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-
-EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = "palannu489@gmail.com"
+EMAIL_HOST_PASSWORD = "gwvrzvoatsenqqkr"
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='palannu489@gmail.com'
-EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
